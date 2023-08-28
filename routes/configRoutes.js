c8b0d02f91e85e0a403c83973b21042e197195c6
ItayMizrachi <itaymiz98@gmail.com>
2023-08-28 16:34:54 +0300

    openai

--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -3,11 +3,13 @@ const path = require("path");
 const indexR = require("./index");
 const usersR = require("./users");
 const toysR = require("./toys");
+const openaiR = require("./openai");
 
 exports.routesInit = (app) => {
   app.use("/", indexR);
   app.use("/users", usersR);
   app.use("/toys", toysR);
+  app.use("/openai", openaiR);
 
   app.get('*', function (req, res) {
     res.status(404).sendFile(path.join(__dirname, "..", "public", "page404.html"));
---
+++ routes/openai.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+router.post("/completions",async (req, res) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${process.env.OPENAI_KEY}`,
+            "Content-Type": "application/json"  // Informing the server that we're sending JSON data
+        },
+        body: JSON.stringify({  // Convert JavaScript object to a JSON string
+            model: "gpt-3.5-turbo",  // Specifying the model to use for the OpenAI request
+            messages: [{ role: "user", content: req.body.message }],  // User's message to get a completion from GPT-3.5
+            max_tokens: 100,  // Limit the response to a maximum of 100 tokens
+        })
+    }
+
+    try {
+        // Making the actual request to the OpenAI API with the given options
+        const response = await fetch(`https://api.openai.com/v1/chat/completions`, options);
+        // Set appropriate CORS headers
+        res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        // Parsing the JSON response from the API
+        const data = await response.json();
+
+        // Sending the received data back to the client
+        res.send(data);
+    } catch (error) {
+        // If there's an error in the try block, log it to the console
+        console.log(error);
+    }
+});
+
+module.exports = router;
